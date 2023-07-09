4c37c4b57339214d2054ca19c948a50bbb0ad1f7
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-09 12:30:17 +0000

    style: 들여쓰기 변경

--- models/user.js
+++ models/user.js
@@ -6,44 +6,44 @@ class User extends Sequelize.Model {
    // 테이블에 대한 설정
    static init(sequelize) {
     return super.init(
-        {  // 첫번째 객체 인수는 테이블 필드에 대한 설정
-          id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-          },
-          email: {
-            type: Sequelize.STRING(40),
-            allowNull: false,
-            unique: true,
-          },
-          userName: {
-            type: Sequelize.STRING(15),
-            allowNull: false,
-          },
-          password: {
-            type: Sequelize.STRING(100),
-            allowNull: false,
-          },
-          snsId: {
-            type: Sequelize.STRING(40),
-            allowNull: true,
-          },
-          provider: {
-            type: Sequelize.STRING(40),
-            allowNull: true
-          }
+      {  // 첫번째 객체 인수는 테이블 필드에 대한 설정
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
         },
-        {  // 두번째 객체 인수는 테이블 자체에 대한 설정
-          sequelize, /* static init 메서드의 매개변수와 연결되는 옵션으로, db.sequelize 객체를 넣어야 한다. */
-          timestamps: true, /* true : 각각 레코드가 생성, 수정될 때의 시간이 자동으로 입력된다. */
-          underscored: false, /* 카멜 표기법을 스네이크 표기법으로 바꾸는 옵션 */
-          modelName: 'User', /* 모델 이름을 설정. */
-          tableName: 'users', /* 데이터베이스의 테이블 이름. */
-          paranoid: false, /* true : deletedAt이라는 컬럼이 생기고 지운 시각이 기록된다. */
-          charset: 'utf8', /* 인코딩 */
-          collate: 'utf8_general_ci'
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+          unique: true,
+        },
+        userName: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        snsId: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+        },
+        provider: {
+          type: Sequelize.STRING(40),
+          allowNull: true
         }
+      },
+      {  // 두번째 객체 인수는 테이블 자체에 대한 설정
+        sequelize, /* static init 메서드의 매개변수와 연결되는 옵션으로, db.sequelize 객체를 넣어야 한다. */
+        timestamps: true, /* true : 각각 레코드가 생성, 수정될 때의 시간이 자동으로 입력된다. */
+        underscored: false, /* 카멜 표기법을 스네이크 표기법으로 바꾸는 옵션 */
+        modelName: 'User', /* 모델 이름을 설정. */
+        tableName: 'users', /* 데이터베이스의 테이블 이름. */
+        paranoid: false, /* true : deletedAt이라는 컬럼이 생기고 지운 시각이 기록된다. */
+        charset: 'utf8', /* 인코딩 */
+        collate: 'utf8_general_ci'
+      }
     );
    }
 
