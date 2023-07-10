8f46ec5ee9b11a2700c94f76841eb5686f755dea
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-10 06:07:15 +0000

    이ㅁ시저장ㅇㅇㅇㅇㅇㅇㅇ

--- routes/auth/auth.ctrl.js
+++ routes/auth/auth.ctrl.js
@@ -35,6 +35,12 @@ exports.input = (req, res) => {
 exports.login = (req, res, next) => {
   console.log("login logic");
   console.log("body seems: ", req.body);
+  
+  // TODO: 카카오, google로 로그인할 때, 해당 소셜 메일을 받아와서 email 항목을 채우고
+  // sns id를 암호화하여 password 필드를 채움
+  // sns 로그인을 시도하는 경우, 해당 sns 계정 이메일 + snsId를 해시화한 password를 통해 local 로그인 시도
+  // TODO: 프론트에서 email 수집 동의 항목 추가
+
   passport.authenticate("local", (authError, user, info) => {
     if(authError) {
       console.error(authError);
@@ -56,21 +62,41 @@ exports.login = (req, res, next) => {
 
 exports.signUp = async (req, res, next) => {
   console.log("singUp logic")
-  const {email, userName, password } = req.body;
+  const { email, userName, password, snsId, provider } = req.body;
   try {
-    // 기존에 이메일로 가입한 사람이 있나 검사 (중복 가입 방지)
+    // 기존에 해당 이메일로 가입한 사람이 있나 검사 (중복 가입 방지)
     const exUser = await User.findOne({ where: { email } });
     if (exUser) {
-       return res.redirect('/signUp?error=exist'); // 에러페이지로 바로 리다이렉트
+      return res.redirect("/signUp?error=exist"); // 에러페이지로 바로 리다이렉트
     }
-    // 정상적인 회원가입 절차면 해시화
-    const hash = await bcrypt.hash(password, 12);
-    // DB에 해당 회원정보 생성
-    await User.create({
-       email,
-       userName,
-       password: hash, // 비밀번호에 해시문자를 넣어준다.
-    });
+
+    // 기존에 해당 snsId로 가입한 사람이 있나 검사 (중복 가입 방지)
+    if (snsId && provider) {
+      const snsExUser = await User.findOne({ where: { snsId, provider } });
+      if (snsExUser) {
+        return res.redirect("/signUp?error=exist");
+      }
+    }
+
+    // local 계정, sns 계정 가입 기록이 모두 없는 경우에만 이후 코드를 실행할 수 있음
+
+    /* local sign up */
+    if (snsId == null) {
+      // 정상적인 회원가입 절차면 해시화
+      const hash = await bcrypt.hash(password, 12);
+      // DB에 해당 회원정보 생성
+      await User.create({
+        email,
+        userName,
+        password: hash, // 비밀번호에 해시문자를 넣어준다.
+      });
+    } else {
+      /* social sign up */
+      await User.create({
+        snsId,
+        provider,
+      });
+    }
     
     req.user = await User.findOne({ where: {email}});
     // initInfo로 넘어간다
--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -1,8 +1,21 @@
-const { UUID } = require('sequelize');
+const { UUID, BaseError } = require('sequelize');
 const UserInfo = require('../../models/userinfo');
 
-exports.postInfo = (req, res) => {
-  { cummulativeTime, todaysTime, points, githubId, badge, uerid}
+exports.postInfo = async (req, res) => {
+  const { cummulativeTime, todaysTime, points, githubId, badge, userId} = req.body;
+  const target = await UserInfo.findOne({where: { userId }})
+  console.log("target is", target);
+  
+  target.set({
+    cummulativeTime: cummulativeTime || target.cummulativeTime,
+    todaysTime: todaysTime || target.todaysTime,
+    points: points || target.points,
+    githubId: githubId || target.githubId,
+    badge: badge || target.badge,
+  })
+
+  console.log("changed result is: ", target);
+  res.status(204).json(JSON.stringify(target));
 }
 
 exports.getInfo = (req, res) => {
